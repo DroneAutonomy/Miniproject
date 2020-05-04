3b74663ce697fe83ac5a41df52509d6807ce07f9
lasseskjotthansen <lasseskjott@gmail.com>
2020-05-04 12:25:13 +0200

    Tensorboard

--- car_classifier_cnn.py
+++ car_classifier_cnn.py
@@ -1,4 +1,6 @@
 import pickle
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
@@ -6,9 +8,16 @@ import os
 import cv2
 import sys
 import math
-import keras
-import tensorflow as tf
 
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
+
+
+"""Name of the model"""
+NAME = "Car-Classifier-cnn-64x2-{}".format(int(time.time()))
+
+"""Log to TensorBoard"""
+tensorboard = TensorBoard(log_dir='/Users/lasseskjotthansen/Desktop/images/logs/{}'.format(NAME))
 
 """Load the data"""
 pickle_in = open("x_train.pickle", "rb")
@@ -21,28 +30,28 @@ y_train_loaded = np.array(y_train_loaded)
 pickle_in.close()
 
 
-x_train_loaded = keras.utils.normalize(x_train_loaded, axis=1)
+x_train_loaded = tf.keras.utils.normalize(x_train_loaded, axis=1)
 
 
-model = keras.models.Sequential()
+model = tf.keras.models.Sequential()
 """Start with the convolutional layer"""
-model.add(keras.layers.Conv2D(64, (3,3), activation=tf.nn.relu, input_shape=x_train_loaded.shape[1:]))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation=tf.nn.relu, input_shape=x_train_loaded.shape[1:]))
 
 """Adding maxpooling layer with size (2,2)"""
-model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
 
 """Doing the same one more time"""
-model.add(keras.layers.Conv2D(64, (3,3), activation=tf.nn.relu))
-model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation=tf.nn.relu))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
 
-model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(128))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128))
 
 """output layer """
-model.add(keras.layers.Dense(197, activation=tf.nn.softmax))
+model.add(tf.keras.layers.Dense(197, activation=tf.nn.softmax))
 
 
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-model.fit(x_train_loaded, y_train_loaded, batch_size=32, validation_split=0.1, epochs=8)
+model.fit(x_train_loaded, y_train_loaded, batch_size=32, validation_split=0.1, epochs=8, callbacks=[tensorboard])
 
